335dadacb392781ad7d83490d6bc6b03d47ac4b9
simonyyc <yangyucheng@csu.fullerton.edu>
2017-02-27 12:17:19 -0800

    Update MedicalRecord.h

--- MedicalRecord.h
+++ MedicalRecord.h
@@ -1,22 +1,17 @@
 #pragma once
 #include <string>
 #include <stdexcept>
-
+#include <fstream>
 #include "Baby.h"
-
 using namespace std;
 
 class MedicalRecord {
 public:
 	// default constructor
-	MedicalRecord() {
-		// TO BE COMPLETED
-	}
+	MedicalRecord();
 
 	// destructor
-	~MedicalRecord() {
-		// TO BE COMPLETED
-	}
+	~MedicalRecord();
 
 	// Load information from a text file with the given filename.
 	void buildMedicalRecordfromDatafile(string filename) {
@@ -38,32 +33,80 @@ public:
 	}
 
 	// return the most frequently appearing name in the text file
-	string mostPopularName() {
-		return "COMPLETE ME"; // TO BE COMPLETED
-	}
+	string mostPopularName();
 
 	// return the number of baby records loaded from the text file
-	int numberOfBirths() {
-		return -1; // TO BE COMPLETED
-	}
+	int numberOfBirths();
 
 	// return the number of babies who had birth weight < 2500 grams
-	int numberOfBabiesWithLowBirthWeight() {
-		return -1; // TO BE COMPLETED
-	}
+	int numberOfBabiesWithLowBirthWeight();
 
 	// return the number of babies who have the name contained in string s
-	int numberOfBabiesWithName(string s) {
-		return -1; // TO BE COMPLETED
-	}
-
+	int numberOfBabiesWithName(string s);
 private:
 	// update the data structure with information contained in Baby object
-	void addEntry(Baby b) {
-		// TO BE COMPLETED
-	}
+	void addEntry(Baby b);
+	int numOfBirths; //use counter in addEntry to determine
+	Baby* dataKeep; //points to dynamic memory array holding the objects
+	int maxBirth; //holds inputted value of maximum entries
 
-	// Add private member variables for your data structure along with any 
-	// other variables required to implement the public member functions
 
 };
+
+MedicalRecord::MedicalRecord() {
+	maxBirth = 100000;
+	dataKeep = new Baby[maxBirth];
+	numOfBirths = 0;
+}//ask user to input maximum number
+
+MedicalRecord::~MedicalRecord() {
+	delete[] dataKeep;
+}//delete the array
+
+string MedicalRecord::mostPopularName() {
+	int maxNameCount = 0, previousMax = 0;
+	string maxName;
+	for (int i = 0; i < numOfBirths; i++) {
+		for (int j = 0; j < numOfBirths; j++) {
+			if (dataKeep[i].getName() == dataKeep[j].getName()) {
+				maxNameCount++;
+			}//two for loops to check for matching names; matched names cause counter to increment
+		}
+		if (maxNameCount > previousMax) {
+			maxName = dataKeep[i].getName();
+			previousMax = maxNameCount;
+			maxNameCount = 0;
+		}//check to see if previous most popular name will be overridden
+
+	}
+	return maxName;
+}
+
+int MedicalRecord::numberOfBirths() {
+	return numOfBirths;
+}//return the numOfBirths based on number of entries added into array
+
+int MedicalRecord::numberOfBabiesWithLowBirthWeight() {
+	int lowBirthWeight = 0;
+	for (int i = 0; i < numOfBirths; i++) {
+		if (dataKeep[i].getWeight() < 2500) {
+			lowBirthWeight++;
+		}
+	}
+	return lowBirthWeight;
+}//loop to see if weight matches criteria
+
+int MedicalRecord::numberOfBabiesWithName(string s) {
+	int numSameName = 0;
+	for (int i = 0; i < numOfBirths; i++) {
+		if (dataKeep[i].getName() == s)
+			numSameName++;
+	}
+	return numSameName;
+}//loop to see if name matches criteria
+
+void MedicalRecord::addEntry(Baby b) {
+	dataKeep[numOfBirths] = b;
+	numOfBirths++;
+}
+
